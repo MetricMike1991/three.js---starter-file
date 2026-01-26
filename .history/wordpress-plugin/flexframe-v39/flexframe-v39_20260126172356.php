1f7db0fd9e1629bbddf801dd0641073b20c82ec8
Michael Shortall <mikeshortall1991@gmail.com>
2026-01-26 17:30:58 +0000

    working mobile UI

--- .history/wordpress-plugin/flexframe-v39/flexframe-v39_20260126172356.php
+++ .history/wordpress-plugin/flexframe-v39/flexframe-v39_20260126172356.php
@@ -1765,7 +1765,7 @@ function flexframe_enqueue_assets() {
         // Register Vite-generated JavaScript bundle (must register before localizing)
         wp_register_script(
             'flexframe-viewer-script',
-            FLEXFRAME_PLUGIN_URL . 'assets/assets/index-CZJMGgte.js',
+            FLEXFRAME_PLUGIN_URL . 'assets/assets/index-D0SwAM-B.js',
             array(),
             FLEXFRAME_VERSION,
             true
