5ef39a1e663f8c73b9034be8148977dd4a12e83c
Michael Shortall <mikeshortall1991@gmail.com>
2026-02-09 10:15:40 +0000

    Build 272: Add V2 menu register_setting + hidden fields + applyPresetSettings handles V2 menu and flat equipment keys

--- .history/wordpress-plugin/flexframe-v40/flexframe-v40_20260209100806.php
+++ .history/wordpress-plugin/flexframe-v40/flexframe-v40_20260209100806.php
@@ -3,7 +3,7 @@
  * Plugin Name: FlexFrame v40
  * Plugin URI: https://flexframe.com
  * Description: 3D interactive exercise viewer with customizable logo and materials
- * Version: 1.41.272
+ * Version: 1.41.271
  * Author: FlexFrame
  * Author URI: https://flexframe.com
  * License: GPL v2 or later
@@ -33,7 +33,7 @@ function flexframe_log($message, $data = null) {
 }
 
 // Define plugin constants
-define('FLEXFRAME_VERSION', '1.41.272');
+define('FLEXFRAME_VERSION', '1.41.271');
 define('FLEXFRAME_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('FLEXFRAME_PLUGIN_URL', plugin_dir_url(__FILE__));
 
@@ -2826,7 +2826,7 @@ function flexframe_enqueue_assets() {
         // Register Vite-generated JavaScript bundle (must register before localizing)
         wp_register_script(
             'flexframe-viewer-script',
-            FLEXFRAME_PLUGIN_URL . 'assets/assets/index-Cj0dDq0f.js',
+            FLEXFRAME_PLUGIN_URL . 'assets/assets/index-ChgrlkNq.js',
             array(),
             FLEXFRAME_VERSION,
             true
